d1af1585c55ed57bad285c1151291a2d64934b11
horacio belardita <horaciobelardita@gmail.com>
2016-08-25 11:37:59 -0300

    Added navigation between pages

--- full_scale.py
+++ full_scale.py
@@ -1,24 +1,30 @@
 import Tkinter as tk
 import ttk
 
-LARGE_FONT = ('System', 14)
+LARGE_FONT = ('Verdana', 12)
 
 
 class App(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
+        # cambiar el tamanio de la ventana
+        self.geometry('800x600')
+        # contenedor para los demas frames
+        container = ttk.Frame(self)
         container.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        # minimum, maximum
+        # configuracion basica(minimum, maximum)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
+        # diccionario para cambiar entre multiples frames
         self.frames = {}
-        frame = StartPage(container, self)
 
-        self.frames[StartPage] = frame
-        frame.grid(row=0, column=0)
+        for f in (StartPage, PageOne, PageTwo):
+
+            frame = f(container, self)
+
+            self.frames[f] = frame
+            frame.grid(row=0, column=0, sticky='nsew')
 
         self.show_frame(StartPage)
 
@@ -26,12 +32,34 @@ class App(tk.Tk):
         frame = self.frames[container]
         frame.tkraise()
 
-class StartPage(tk.Frame):
+class PageOne(ttk.Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        ttk.Frame.__init__(self, parent)
+        ttk.Label(self, text='Page One', font=LARGE_FONT).pack(padx=10, pady=10)
+        ttk.Button(self, text='Visit page 2',
+        command=lambda: controller.show_frame(PageTwo)).pack()
+        ttk.Button(self, text='Back to Home',
+        command=lambda: controller.show_frame(StartPage)).pack()
+
+class PageTwo(ttk.Frame):
+
+    def __init__(self, parent, controller):
+        ttk.Frame.__init__(self, parent)
+        ttk.Label(self, text='Page Two', font=LARGE_FONT).pack(padx=10, pady=10)
+        ttk.Button(self, text='Visit Page 1',
+        command=lambda: controller.show_frame(PageOne)).pack()
+        ttk.Button(self, text='Back to Home',
+        command=lambda: controller.show_frame(StartPage)).pack()
+
+
+class StartPage(ttk.Frame):
+
+    def __init__(self, parent, controller):
+        ttk.Frame.__init__(self, parent)
         ttk.Label(self, text='Start Page', font=LARGE_FONT).pack(padx=10, pady=10)
-        
+        ttk.Button(self, text='Visit Page 1',
+                command=lambda: controller.show_frame(PageOne)).pack()
 
 if __name__ == '__main__':
     app = App()
--- grade_book.py
+++ grade_book.py
@@ -5,11 +5,43 @@ import tkMessageBox, tkFileDialog
 # file format
 # student name, grade, grade
 
+class AddNameFrame(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        ttk.Label(self, text='CREAR ALUMNOS').grid(row=0, columnspan=2)
+
+
 
 class GradeFrame(tk.Frame):
 
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        ttk.Label(self, text='LIBRO DE NOTAS', font=('System', 14, 'bold')).pack()
+        ttk.Button(self, text='Crear', command=self.create).pack(pady=10)
+        ttk.Button(self, text='Agregar', command=self.add, state='disabled').pack(pady=10)
+        ttk.Button(self, text='Ver', command=self.show, state='disabled').pack(pady=10)
+        ttk.Button(self, text='Eliminar Nota', command=self.delete_grade, state='disabled').pack(pady=10)
+        ttk.Button(self, text='Eliminar Alumno', command=self.delete_student, state='disabled').pack(pady=10)
+        ttk.Button(self, text='Salir', command=controller.destroy).pack(pady=10)
+        self.pack()
+
+    def add(self):
+        pass
+
+    def show(self):
+        pass
+
+    def create(self):
+        _file = tkFileDialog.asksaveasfile()
+        if _file:
+            names = []
+
+    def delete_grade(self):
+        pass
+
+    def delete_student(self):
+        pass
 
 
 class App(tk.Tk):
@@ -17,8 +49,22 @@ class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title('GRADE BOOK')
-        self.resizable(False, False)
-        login_frame = LoginFrame(self)
+        container = tk.Frame(self)
+        container.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        # minimum, maximum
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        frame = GradeFrame(container, self)
+        self.frames[GradeFrame] = frame
+
+        frame.grid(column=0, row=0)
+        self.show_frame(GradeFrame)
+
+    def show_frame(self, container):
+        frame = self.frames[container]
+        frame.tkraise()
 
 
 if __name__ == '__main__':
